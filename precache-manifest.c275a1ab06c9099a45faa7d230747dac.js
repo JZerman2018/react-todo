4235b65d08f6789d10ea22c60225b7695e3ec781
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-01-27 22:18:57 -0500

    Updates

---
+++ precache-manifest.c275a1ab06c9099a45faa7d230747dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "264e6e707b05215b66e4",
+    "url": "/react-todo/static/js/main.264e6e70.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/react-todo/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "264e6e707b05215b66e4",
+    "url": "/react-todo/static/css/main.e086587c.chunk.css"
+  },
+  {
+    "revision": "469b8a8326af68b43421a5ef70bb0aef",
+    "url": "/react-todo/index.html"
+  }
+];
